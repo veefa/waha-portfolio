11b7fcaee95aa9675fefa046997ceabaaa1ef9a8
vefa <44091688+veefa@users.noreply.github.com>
2025-07-31 18:59:26 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <div className="bg-red-500 p-4 text-white">Tailwind is working!</div>
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
